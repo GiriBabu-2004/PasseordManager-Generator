844f469b2cf46729fbed07f226df92c9faaf69cc
GiriBabu-2004 <157339767+GiriBabu-2004@users.noreply.github.com>
2025-04-26 13:09:20 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', form);
+      const res = await axios.post('https://genlock-backend.onrender.com', form);
       localStorage.setItem('token', res.data.token);
       navigate('/dashboard');
     } catch (err) {
